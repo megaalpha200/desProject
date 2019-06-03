5ff6696e45a0211f0ed50ed954a898a5e1b87340
Jose A. Alvarado <megaalpha200@gmail.com>
2019-06-02 20:33:26 -0500

    Restructured Kotlin Project Directories and more
    *Restructured Kotlin project directories
    *Removed Kotlin restrictions on .gitignore
    *Removed bitmanip package from Kotlin project.

--- kotlin/des/src/DESFunctions/desMainFunctions.kt
+++ kotlin/des/src/DESFunctions/desMainFunctions.kt
@@ -2,7 +2,6 @@ package DESFunctions
 
 import TextConversions.convertBinToHex
 import TextConversions.convertHexToBin
-import binmanip.shiftBitsLeft
 import java.lang.StringBuilder
 
 /*--------------------------------P-Boxes-------------------------------------*/
@@ -283,6 +282,38 @@ fun applySBox(input: String, sBox: Array<IntArray>) : String {
     val boxCol = Integer.parseInt(input.substring(1, input.lastIndex), 2)
 
     return Integer.toBinaryString(sBox[boxRow][boxCol]).padStart(4, '0')
+}
+
+fun shiftBitsLeft(input: String, shiftBy: Int = 1) : String {
+    val modShiftVal = shiftBy % (input.length)
+    val shiftedString = StringBuilder("")
+
+    shiftedString.append(input.substring(modShiftVal))
+
+    for(i in 0..(modShiftVal - 1)) {
+        shiftedString.append(input[i])
+    }
+
+    return shiftedString.toString()
+}
+
+fun binSplit(input: String, blockSize: Int) : List<String> {
+    var binBlocksList = input.chunked(blockSize)
+    val lastBlock = binBlocksList[binBlocksList.lastIndex]
+
+    if (lastBlock.length < blockSize) {
+        val tempList = arrayListOf<String>()
+
+        for (i in 0..(binBlocksList.lastIndex - 1)) {
+            tempList.add(binBlocksList[i])
+        }
+
+        tempList.add(lastBlock.padEnd(blockSize, '0'))
+
+        binBlocksList = tempList
+    }
+
+    return binBlocksList
 }
 
 /*----------------------------------------------------------------------------------*/
